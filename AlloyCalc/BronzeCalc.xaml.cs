51296605de30b735c5862fbc23d193095a6f3faa
LeviSY <levichannel38@gmail.com>
2024-03-14 17:54:29 +0300

    some changes

--- AlloyCalc/BronzeCalc.xaml.cs
+++ AlloyCalc/BronzeCalc.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using System.Printing;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace AlloyCalc
 {
@@ -42,6 +43,16 @@ namespace AlloyCalc
             AlloySelection.Items.Add("Brass");
         }
 
+        private void metal1_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double slider1 = metal1_slider.Value;
+            double slider2 = metal2_slider.Value;
+            double slider3 = metal3_slider.Value;
 
+            if (slider1+slider2 != (double)100)
+            {
+                metal2_slider.Value = (double)100 - slider1;
+            }
+        }
     }
 }
